0c67f9ef9b0c40c6c6269b5edf4974c20b8c5d17
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-07-05 15:45:31 +0530

    added shadow header, ingrediets

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,13 @@ const linkAction = () => {
 navLink.forEach(n => n.addEventListener('click', linkAction));
 /*=============== ADD SHADOW HEADER ===============*/
 
+const shadowHeader = () => {
+    const header = document.getElementById('header');
+
+    this.scrollY >= 50 ? header.classList.add('shadow-header') : header.classList.remove('shadow-header');
+}
+
+window.addEventListener('scroll', shadowHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
 
