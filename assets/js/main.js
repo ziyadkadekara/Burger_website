1705c5e0e89b2be0173720e261ac560125ff3c9b
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-07-09 20:26:56 +0530

    scroll section active link done

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,5 +43,21 @@ window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections = document.querySelectorAll('section[id]');
 
+const scrollActivate = () => {
+    const scrollDown = window.scrollY;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) { sectionsClass.classList.add('active-link') } else {
+            sectionsClass.classList.remove('active-link');
+        }
+    });
+}
+window.addEventListener('scroll', scrollActivate);
 /*=============== SCROLL REVEAL ANIMATION ===============*/
