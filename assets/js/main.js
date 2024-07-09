5be3a490a0608c103a8d85a941ddfbf4f3d3af61
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-07-09 20:06:51 +0530

    added scrol up animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,6 +34,12 @@ window.addEventListener('scroll', shadowHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
